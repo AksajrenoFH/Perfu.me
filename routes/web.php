250176ac49e44185aeba3b17bb8cff3ec4966a37
Faiz Nabil <faiznabilakram.xpro@gmail.com>
2026-08-11 14:11:40 +0700

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/refill', [RefillController::class, 'index'])->name('refill');
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
-
+`
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
