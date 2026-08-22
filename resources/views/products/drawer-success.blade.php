a0b297ef2fea917c36320f586fad4f3498d330b2
Faiz Nabil <faiznabilakram.xpro@gmail.com>
2026-08-22 15:12:11 +0700

    update home

--- resources/views/products/drawer-success.blade.php
+++ resources/views/products/drawer-success.blade.php
@@ -5,7 +5,6 @@
 </head>
 <body>
     <script>
-        // Mengirimkan event 'product-saved' ke halaman Parent (Tabel)
         window.parent.postMessage('product-saved', '*');
     </script>
 </body>
